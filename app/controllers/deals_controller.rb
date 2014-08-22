67d51f4c8eed74be2a8e03eb54b2cced76555a07
kennytrionfo <kentrionfo@gmail.com>
2014-08-21 20:48:06 -0600

    finished index action with simple api http get, then assigning specifically parsed data to instance variables to use in view

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,40 +1,18 @@
 class DealsController < ApplicationController
     def index
-      @deals = HTTParty.get('http://api.popshops.com/v3/deals.json?account=etl4xy4bqez70idzyypjwyfxn&catalog=dxeqyv83t3vcox7iztg9ufg16')
-      # Deal.title = popshops_deal['description']
-      # Deal.description = popshops_deal['name']
-
-      # popshops_deal = Deal.new
-
-     #@deals = Deal.all
+      deal = HTTParty.get('http://api.popshops.com/v3/deals.json?account=etl4xy4bqez70idzyypjwyfxn&catalog=dxeqyv83t3vcox7iztg9ufg16')
+      @description = deal['results']['deals']['deal'][0]['description']
+      @name = deal['results']['deals']['deal'][0]['name']
+      @restrictions = deal['results']['deals']['deal'][0]['restrictions']
     end
+
 
     def show
      @deal = Deal.find(params[:id])
     end
 
+
     def new
      @deal = Deal.new
     end
 end
-
-
-
-# Psuedo
-# Go get api deals
-# apiDeals = api.httparty.get('')
-#
-# @apiDeals is some json object, map the data
-# newDbDeal = model.deal()
-# newDbDeal.name = @apiDeals[0].name
-#
-# Save to db
-# newDbDeal.save();
-#
-# Get it again for the view
-# viewDbDeals = model.getDbDeals()
-
-# t.string :title
-# t.text :description
-# t.float :number
-# t.decimal :price
