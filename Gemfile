fe38f4a3c28d6293a2701b960415ca41bc2a33f4
kennytrionfo <kentrionfo@gmail.com>
2014-08-21 19:44:32 -0600

    playing around with index action. putting api stuff in

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 
 gem 'popshops'
+gem 'rakuten'
+
+gem 'httparty'
 
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,13 +1,40 @@
 class DealsController < ApplicationController
-  def index
-   @deals = Deal.all
-  end
+    def index
+      @deals = HTTParty.get('http://api.popshops.com/v3/deals.json?account=etl4xy4bqez70idzyypjwyfxn&catalog=dxeqyv83t3vcox7iztg9ufg16')
+      # Deal.title = popshops_deal['description']
+      # Deal.description = popshops_deal['name']
 
-  def show
-   @deal = Deal.find(params[:id])
-  end
+      # popshops_deal = Deal.new
 
-  def new
-   @deal = Deal.new
-  end
+     #@deals = Deal.all
+    end
+
+    def show
+     @deal = Deal.find(params[:id])
+    end
+
+    def new
+     @deal = Deal.new
+    end
 end
+
+
+
+# Psuedo
+# Go get api deals
+# apiDeals = api.httparty.get('')
+#
+# @apiDeals is some json object, map the data
+# newDbDeal = model.deal()
+# newDbDeal.name = @apiDeals[0].name
+#
+# Save to db
+# newDbDeal.save();
+#
+# Get it again for the view
+# viewDbDeals = model.getDbDeals()
+
+# t.string :title
+# t.text :description
+# t.float :number
+# t.decimal :price
